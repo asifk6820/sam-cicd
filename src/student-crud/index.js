73dc0e0b22e010d0975f82485938c5eefef03795
Mohd Asif KHAN <mohdk.in@mouritech.com>
2023-01-24 13:10:03 +0530

    adding student crud

---
+++ src/student-crud/index.js
@@ -0,0 +1,38 @@
+export const handler = async (event, context) => {
+    let body;
+    let statusCode = 200;
+    const headers = {
+      "Content-Type": "application/json",
+    };
+  
+    try {
+      switch (event.path) {
+        case "/deleteStudent/{id}":
+          body = `Deleted student ${event.pathParameters.id}`;
+          break;
+        case "/updateStudent/{id}":
+          body = "Dummy data";
+          break;
+        case "/readStudent":
+          body = "Dummy data";
+          break;
+        case "/createStudent":
+          let requestJSON = JSON.parse(event.body);
+          body = `Put student ${requestJSON.id}`;
+          break;
+        default:
+          throw new Error(`Unsupported route: "${event.routeKey}"`);
+      }
+    } catch (err) {
+      statusCode = 400;
+      body = err.message;
+    } finally {
+      body = JSON.stringify(body);
+    }
+  
+    return {
+      statusCode,
+      body,
+      headers,
+    };
+  };
